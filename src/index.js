6a3ce7e751843c192d3ac4eb70cb0c93e0a1e9e8
Lisa DeSpain <lisamdespain@gmail.com>
2022-05-17 15:09:18 -0500

    yargh

--- src/index.js
+++ src/index.js
@@ -9,9 +9,18 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+let store
+export const resetStore = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  store = createStore(reducer, composeEnhancers())
+}
+resetStore()
+
 ReactDOM.render(
   <Router>
-      <App />
+  <Provider store={store}>
+    <App />
+  </Provider>
   </Router>,
   document.getElementById('root')
 );
